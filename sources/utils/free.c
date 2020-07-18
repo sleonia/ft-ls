d58d3ad0e0ff4ac2c706fd8d44ab3fedf105a98d
Carly Yuriko <cyuriko@oa-k1.msk.21-school.ru>
2020-07-18 19:53:56 +0300

    add freeing things

---
+++ sources/utils/free.c
@@ -0,0 +1,29 @@
+#include "types.h"
+#include "utils.h"
+#include "libft.h"
+
+static void free_file(t_file *file)
+{
+	if (!file)
+		return ;
+	if (file->files_inside)
+		free_file(file->files_inside);
+	if (file->next)
+		free_file(file->next);
+	ft_strdel(&file->name);
+	ft_strdel(&file->full_path);
+	if (file->fd)
+		closedir(file->fd);
+	free(file);
+	file = NULL;
+}
+
+void free_all(t_file *files, t_conf *conf, t_flags *flags)
+{
+
+	if (conf)
+		free(conf);
+	if (flags)
+		free(flags);
+	free_file(files);
+}
