13b9b8fdb17eb22ac1052ac9ea7813ebcd238242
sleonia <sleonia@student.21-school.ru>
2020-07-18 20:09:27 +0300

    Fixed sort

--- sources/sort/reverse.c
+++ sources/sort/reverse.c
@@ -4,47 +4,53 @@
 
 static void	reverse1(t_file **files)
 {
-	t_file *tmp;
+	t_file **tmp;
 	t_file *prev;
 	t_file *next;
 //
 	if (!files || !*files)
 		return ;
-	tmp = *files;
+	tmp = files;
 	prev = NULL;
 	while (tmp)
 	{
-		next = tmp->next;
-		prev = tmp->prev;
-		tmp->next = prev;
-		tmp->prev = next;
-		tmp = next;
+		//printf("%s\n", tmp->name);
+		next = (*tmp)->next;
+		prev = (*tmp)->prev;
+		(*tmp)->next = prev;
+		(*tmp)->prev = next;
+		if (!next)
+		{
+			//printf("%s\n", (*files)->name);
+			//printf("%s\n", (*files)->next->name);
+			break ;
+		}
+		(*tmp) = next;
 	}
-	printf("%p\n", tmp);
 }
 
 
 void	reverse(t_file **files)
 {
-	t_file *tmp;
+	t_file **tmp;
 	t_file *prev;
 	t_file *next;
-
-	printf("%p\n", tmp);
-
 //
-	//if (!files || !*files)
-	//	return ;
-	tmp = *files;
+	if (!files || !*files)
+		return ;
+	tmp = files;
 	prev = NULL;
 	while (tmp)
 	{
-		reverse1(&tmp->files_inside);
-		next = tmp->next;
-		prev = tmp->prev;
-		tmp->next = prev;
-		tmp->prev = next;
-		tmp = next;
+		reverse1(&(*tmp)->files_inside);
+		next = (*tmp)->next;
+		prev = (*tmp)->prev;
+		(*tmp)->next = prev;
+		(*tmp)->prev = next;
+		if (!next)
+		{
+			break ;
+		}
+		(*tmp) = next;
 	}
-	printf("%p\n", tmp);
 }
--- sources/sort/sort.c
+++ sources/sort/sort.c
@@ -7,10 +7,10 @@
 ** \brief
 */
 
-static void front_back_split(t_file* head, t_file** left, t_file** right)
+static void	front_back_split(t_file *head, t_file **left, t_file **right)
 {
-	t_file* slow;
-	t_file* fast;
+	t_file *slow;
+	t_file *fast;
 
 	slow = head;
 	fast = head->next;
@@ -28,9 +28,9 @@ static void front_back_split(t_file* head, t_file** left, t_file** right)
 	slow->next = NULL;
 }
 
-static t_file*	sorted_merge_by_time(t_file* left, t_file* right)
+static t_file	*sorted_merge_by_time(t_file *left, t_file *right)
 {
-	t_file* res;
+	t_file *res;
 
 	res = NULL;
 	if (!left)
@@ -47,22 +47,17 @@ static t_file*	sorted_merge_by_time(t_file* left, t_file* right)
 		res = left;
 		res->next = sorted_merge_by_time(left->next, right);
 	}
-	else if (left->time == right->time)
-	{
-		res = right;
-		res->next = sorted_merge_by_time(left, right->next);		
-	}
 	else
 	{
 		res = right;
 		res->next = sorted_merge_by_time(left, right->next);
 	}
-	return res;
+	return (res);
 }
 
-static t_file*	sorted_merge_by_ascii(t_file* left, t_file* right)
+static t_file	*sorted_merge_by_ascii(t_file *left, t_file *right)
 {
-	t_file* res;
+	t_file *res;
 
 	res = NULL;
 	if (!left)
@@ -79,19 +74,18 @@ static t_file*	sorted_merge_by_ascii(t_file* left, t_file* right)
 		res = right;
 		res->next = sorted_merge_by_ascii(left, right->next);
 	}
-	return res;
+	return (res);
 }
 
-void	merge_sort(bool is_ascii, bool is_time, t_file** head)
+void	merge_sort(bool is_ascii, bool is_time, t_file **head)
 {
-	t_file* tmp;
-	t_file* left;
-	t_file* right;
+	t_file *tmp;
+	t_file *left;
+	t_file *right;
 
 	tmp = *head;
-	if (!tmp || !tmp->next) {
+	if (!tmp || !tmp->next)
 		return ;
-	}
 	front_back_split(tmp, &left, &right);
 	merge_sort(is_ascii, is_time, &left);
 	merge_sort(is_ascii, is_time, &right);
@@ -101,16 +95,12 @@ void	merge_sort(bool is_ascii, bool is_time, t_file** head)
 		*head = sorted_merge_by_time(left, right);
 }
 
-/*!
-** https://www.geeksforgeeks.org/merge-sort-for-linked-list/
-*/
-
 void		sort(const t_flags *flags, t_file *files)
 {
 	if (flags->t)
 		merge_sort(false, flags->t, &(files->files_inside));
 	else if (flags->little_r)
 		reverse(&files);
-	else
+	else if (!flags->f)
 		merge_sort(true, false, &(files->files_inside));
 }
